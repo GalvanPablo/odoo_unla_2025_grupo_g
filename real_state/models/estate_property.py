26d1599336416544d4e031c160269932e7206081
csantander93 <casdesarrollo.2022@gmail.com>
2025-09-09 08:51:14 -0300

    se realizaron los ejercicios 18 al 23 inclusive

--- real_state/models/estate_property.py
+++ real_state/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import models, fields
+from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -7,16 +8,23 @@ class EstateProperty(models.Model):
     name = fields.Char(string="Título", required=True)
     description = fields.Text(string="Descripción")
     postcode = fields.Char(string="Código postal")
-    date_availability = fields.Date(string="Fecha disponibilidad")
+    date_availability = fields.Date(
+        string="Fecha disponibilidad",
+        copy=False,
+        default=lambda self: fields.Date.context_today(self) + relativedelta(months=3)
+    )
     expected_price = fields.Float(string="Precio esperado")
-    selling_price = fields.Float(string="Precio de venta")
+    selling_price = fields.Float(
+        string="Precio de venta",
+        copy=False
+    )
     bedrooms = fields.Integer(string="Habitaciones", default=2)
     living_area = fields.Integer(string="Superficie cubierta")
     facades = fields.Integer(string="Fachadas")
     garage = fields.Boolean(string="Garage")
     garden = fields.Boolean(string="Jardín")
     garden_orientation = fields.Selection(
-        string='Orientación del jardín',
+        string="Orientación del jardín",
         selection=[
             ('north', 'Norte'),
             ('south', 'Sur'),
@@ -26,3 +34,18 @@ class EstateProperty(models.Model):
         default="north"
     )
     gardenArea = fields.Integer(string="Superficie jardín")
+    
+    # Campo state - Ejercicio 21
+    state = fields.Selection(
+        selection=[
+            ("new", "Nuevo"),
+            ("offer_received", "Oferta recibida"),
+            ("offer_accepted", "Oferta aceptada"),
+            ("sold", "Vendido"),
+            ("canceled", "Cancelado"),
+        ],
+        string="Estado",
+        required=True,
+        copy=False,
+        default="new",
+    )
