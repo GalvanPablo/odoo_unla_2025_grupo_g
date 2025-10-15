84069f2721a3980ead8058e815bd309ce59ad80c
Pablo Galvan <pablogalvan.015@gmail.com>
2025-10-15 18:39:39 -0300

    U2 - Ejercicio 26 y 27 - Facturar propiedad vendida

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Inmobiliaria - Facturación",
+    "version": "1.0",
+    "author": "UNLa-Grupo-G",
+    "category": "UNLa",
+    "description": "Modulo creado como complemento y enlace de real_state para realizar la facturación",
+    "depends": [
+        "real_state",
+        "account",
+        "sale"
+    ],
+    "data": [
+        
+    ],
+}
